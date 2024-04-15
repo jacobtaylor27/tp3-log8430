d10d8f95c40455320e072422ce57b0a259e8a304
Jacob Taylor <jacob.taylor.01@gmail.com>
2024-04-15 12:53:28 -0400

    remove cleanup dir

--- script.py
+++ script.py
@@ -46,7 +46,7 @@ def main():
         print("Invalid write ratio. Please use a float between 0 and 1")
         return 1
     
-    clean_directories()
+    # clean_directories()
     generate_docker_compose()
     generate_workload()
     run_docker_compose()
